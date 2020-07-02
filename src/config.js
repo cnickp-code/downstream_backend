c733bec9d67a7786a9c3e0bc9b2d2ef5a6fda037
Carlo Paredes <c.nick.paredes@gmail.com>
2020-07-02 13:22:49 -0700

    Changed database_url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/downstream',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/downstream_test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/downstream',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/downstream_test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
